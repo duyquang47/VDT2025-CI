842cc1b505ed622177baa7e42de11874b8b3b78d
Administrator <admin@example.com>
2025-06-23 13:15:06 +0700

    Update log

--- app/vote/app.py
+++ app/vote/app.py
@@ -15,11 +15,13 @@ hostname = socket.gethostname()
 app = Flask(__name__)
 
 logger = logging.getLogger("vote-app")
-logger.setLevel(logging.INFO)
+
 logHandler = logging.StreamHandler()
 formatter = jsonlogger.JsonFormatter('%(asctime)s %(name)s %(levelname)s %(message)s')
 logHandler.setFormatter(formatter)
 logger.addHandler(logHandler)
+app.logger.addHandler(logHandler)
+app.logger.setLevel(logging.INFO)
 
 @app.after_request
 def log_request_info(response):
